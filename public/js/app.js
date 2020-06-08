6184a7535c00bf8fcebaf3deda6c03d3bb50b3c7
StrokeOfGeniusX <adeolaonads@gmail.com>
2020-06-08 20:22:56 +0100

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client-side JavaScript is loaded')
-console.log('Client-side JavaScript is loaded a second time')
-
 // fetch('https://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
@@ -8,14 +5,6 @@ console.log('Client-side JavaScript is loaded a second time')
 // })
 
 
-// fetch('http://localhost:3000/weather?address=!').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error){
-//             console.log(data.error)
-//         } else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
         
 //     })
 // })
